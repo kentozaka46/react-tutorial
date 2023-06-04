55eded2a06dd0989a58b227608d5799e10d6dcb6
金井建斗 <macos@macosnoMBP.lan>
2023-06-04 12:08:22 +0900

    update:三目並べボードにする

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 
-export default function Square() {
+export default function Board() {
   // JSX要素（表示する内容を記述するJavaScriptコードとHTMLタグの組み合わせ）
-  return <button className="square">X</button>;
+  return (
+    <div>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </div>
+  );
 }
